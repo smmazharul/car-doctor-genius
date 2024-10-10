b383982f5b6539dd9b74116cb7cde2da75b73f0d
smmazharul <hmoxygen@gmail.com>
2024-10-10 22:20:49 +0600

    get data by id first find id using params and make this id objectid and finone data using this id and send the result

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -42,7 +42,14 @@ async function run() {
           const cursor = serviceCollection.find();
           const result = await cursor.toArray();
           res.send(result)
-    })
+      })
+      
+      app.get("/services/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await serviceCollection.findOne(query)
+            res.send(result)
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
--- front-end/src/Pages/CheckOut/CheckOut.jsx
+++ front-end/src/Pages/CheckOut/CheckOut.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CheckOut = () => {
     return (
         <div>
-            <h2>Book Service</h2>
+            <h2>Book Service:</h2>
         </div>
     );
 };
