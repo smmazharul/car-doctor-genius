8a7ada816ebbe621dc552040804a997cfabc82b4
smmazharul <hmoxygen@gmail.com>
2024-10-11 17:00:29 +0600

    create token and set invironment variavble and access in server using process.env.--name-- in sign()  secret position

--- back-end/index.js
+++ back-end/index.js
@@ -45,7 +45,9 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       console.log(user)
-      const token = jwt.sign(user,'secret',{expiresIn:'1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
       res.send(token);
     })
     
