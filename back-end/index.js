9fcc6e59eacf3781e1c70c1c32b78c94f8f5def6
smmazharul <hmoxygen@gmail.com>
2024-10-11 12:53:42 +0600

    get bookings data by email id

--- back-end/index.js
+++ back-end/index.js
@@ -52,6 +52,18 @@ async function run() {
           res.send(result);
       })
 
+
+      // bookings
+      app.get('/bookings',async(req,res)=>{
+        console.log(req.query.email)
+        let query = {};
+        if(req.query?.email){
+          query={email:req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+      })
+
       app.post("/bookings", async (req, res) => {
           const booking = req.body;
           const result = await bookingCollection.insertOne(booking)
